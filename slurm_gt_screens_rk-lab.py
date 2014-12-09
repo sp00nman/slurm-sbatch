ca1f0480c699693cdcd154f37e300f2ff81dfa79
Fiorella Schischlik <fiorella.schischlik@gmail.com>
2014-12-09 14:48:22 +0100

    added command line menu to submit script

--- slurm_gt_screens_rk-lab.py
+++ slurm_gt_screens_rk-lab.py
@@ -1,50 +1,71 @@
 #!/cm/shared/apps/python/2.7.6/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
 import os
-from slurm.slurm_header import slurm_header
+import argparse
+import getpass
 from slurm.slurm_parse_sample_file import slurm_tab_sample_files
 from slurm.slurm_write_sh_files import write_sh_files
 
-if __name__ == "__main__":
 
-    def slurm_cmd(sample_dir, sample_name, screen_name, ref_genome):
+def slurm_cmd(sample_dir, sample_name, screen_name, ref_genome):
         
-        cmd = "BaF3_screen_workflow.sh " + "\\\n" \
-            + "-i " + sample_dir + "/" + sample_name + " \\\n" \
-            + "-s " + screen_name + " \\\n" \
-            + "-g " + ref_genome + "\n"
+    cmd = "BaF3_screen_workflow.sh " + "\\\n" \
+        + "-i " + sample_dir + "/" + sample_name + " \\\n" \
+        + "-s " + screen_name + " \\\n" \
+        + "-g " + ref_genome + "\n"
 
-        return cmd
-                                                
-    # command line arguments
-    sample_file = sys.argv[1]
-    ntasks = sys.argv[2]
-    cpus_per_task = sys.argv[3]
-    mem = sys.argv[4]
-    time = sys.argv[5]
-    queue = sys.argv[6]
-    account = sys.argv[7]
-    output_dir = sys.argv[8]
-    prefix = sys.argv[9]
-    
-    # defaults
-    if not cpus_per_task:
-        cpus_per_task = 4
-    if not mem:
-        mem = 6000
-    if not time:
-        time = "10:0:0"
-    if not queue:
-        queue = mediumq
-    if not account:
-        account = fschischlik
-    if not output_dir:
-        output_dir = os.getcwd()
-    
+    return cmd
+
+
+if __name__ == '__main__':
+    # required arguments
+    parser = argparse.ArgumentParser(description='slurm_gt_screens_rk-lab.py')
+    parser.add_argument('--debug', required=False, type=int, help='Debug level')
+    parser.add_argument('--sample_file', required=True, type=str,
+                        help='sample file FORMAT: ')
+    parser.add_argument('--prefix', required=False, type=str,
+                        help="prefix of output file")
+    parser.add_argument('--output_dir', required=False, type=str,
+                        help="output_dir")
+    parser.add_argument('--cpus_per_task', required=False, type=str, default=1,
+                        help='number of cpus per task (default: %(default)s)')
+    parser.add_argument('--ntasks', required=False, type=str, default=1,
+                        help='number of tasks (default: %(default)s)')
+    parser.add_argument('--mem', required=False, type=str, default=8000,
+                        help='memory usage in MB (default: %(default)s)')
+    parser.add_argument('--time', required=False, type=str, default="2:00:00",
+                        help='FORMAT: HH:MM:SS (default: %(default)s)')
+    parser.add_argument('--queue', required=False, type=str, default="mediumq",
+                        choices=["shortq", "mediumq", "longq"],
+                        help='cluster queue/partition (default: %(default)s)')
+    parser.add_argument('--account', required=False, type=str, help='username')
+
+    args = parser.parse_args()
+
+    if not args.cpus_per_task:
+        args.cpus_per_task = str("4")
+    if not args.ntasks:
+        args.ntasks = str("1")
+    if not args.mem:
+        args.mem = str("6000")
+    if not args.time:
+        args.time = "10:0:0"
+    if not args.queue:
+        args.queue = "mediumq"
+    if not args.account:
+        args.account = getpass.getuser()
+    if not args.output_dir:
+        args.output_dir = os.getcwd()
+    if not args.sample_dir:
+        args.sample_dir = os.getcwd()
+    if not args.prefix:
+        args.prefix = "SAMPLE"
+
+    print args
+
     #run
-    samples = slurm_tab_sample_files(sample_file)
+    samples = slurm_tab_sample_files(args.sample_file)
 
     for each_sample in samples:
         job_name = each_sample[0]
@@ -54,6 +75,6 @@ if __name__ == "__main__":
             screen_name=each_sample[0], ref_genome=each_sample[2])
         print job_name
         print cmd
-        write_sh_files(job_name, ntasks, cpus_per_task, mem, 
-                       time, queue, account,
-                       output_dir, prefix, cmd)
+        write_sh_files(job_name, args.ntasks, args.cpus_per_task, args.mem,
+                       args.time, args.queue, args.account,
+                       args.output_dir, args.prefix, cmd)
